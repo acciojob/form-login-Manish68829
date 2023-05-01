44a537f032b5a2cf78dc9b0d990e0b7418a5325a
Manish <121147194+Manish68829@users.noreply.github.com>
2023-05-01 23:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+let c= document.querySelector(".inpu");
+      console.log(c.innerText);
+    let d= document.querySelector(".input");
+   function getFormvalue() {
     //Write your code here
+
+	alert( c.value+" "+ d.value);
 
 }
